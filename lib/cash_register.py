a4c12b30936efbd7d4b31f6b6ce90288b97b051f
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-02-23 08:49:44 -0700

    first deliverable done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+  total = 0
+  def __init__(self, discount=0):
+    self.discount = discount
+
+
+  def add_item(self, title, price, quantity=1):
+    self.total += price * quantity
+    self.title = title
+    
+
+

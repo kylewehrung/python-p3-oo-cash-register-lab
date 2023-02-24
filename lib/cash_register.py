6f5f9840154c7020630ed50c418750cca59da3af
Bianca Aspin <bianca@mj94.net>
2023-02-24 08:42:05 -0800

    Complete second deliverable - calculate discounts.

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -10,6 +10,15 @@ class CashRegister:
   def add_item(self, title, price, quantity=1):
     self.total += price * quantity
     self.title = title
+
+  def apply_discount(self):
+    percentage = self.discount/100
+    if self.discount > 0:
+      subtract = self.total * percentage
+      self.total -= subtract
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
     
 
 
